b0ee5d80cf14436b740aff0b0e02ff95b996d9fb
lizzaaabb <lbogveradze12@gmail.com>
2026-04-01 15:01:25 +0400

    bottom 90

---
+++ src/components/services.js
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import '../styles/services'
+
+function services() {
+  return (
+    <div className='services-container'>
+      
+    </div>
+  )
+}
+
+export default services
