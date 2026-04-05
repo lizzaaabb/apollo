b8f850f0c99460c5dc7c53562eecb675a0aab82a
lizzaaabb <lbogveradze12@gmail.com>
2026-04-05 13:22:57 +0400

    f

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,18 @@
 import Header from '../components/header'
 
-
 export const metadata = {
   title: "Apollo Creations - საიტის დამზადება",
   description: "Best web development services for your business. We create stunning websites that drive results. Contact us today to get started!",
-};
+  themeColor: '#1a0f3a',
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" style={{ backgroundColor: '#1a0f3a' }}>
+      <body style={{ backgroundColor: '#1a0f3a' }}>
         <Header />
         <div>{children}</div>
       </body>
     </html>
-  );
+  )
 }
